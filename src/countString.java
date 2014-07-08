0e75966e37eb25b6007ea13223c13f20ea156504
Raghav Bhatnagar <Raghav_Bhatnagar@intuit.com>
2014-07-08 13:54:53 -0700

    Added code comments to countString.java

--- src/countString.java
+++ src/countString.java
@@ -17,20 +17,33 @@
 
 public class countString {
     private static int countStrings(int n) {
+        // a[i] be the number of binary strings of length i which do not contain any two consecutive 1’s
         int[] a = new int[n];
+        // b[i] be the number of binary strings of length i which do not contain any two consecutive 0’s
         int[] b = new int[n];
+        // Base case
         a[0] = b[0] = 1;
 
         for (int i=1; i<n ; i++) {
             a[i] = b[i-1];
             b[i] = a[i-1] + b[i-1];
         }
+        // The total number of strings of length i is just a[i] + b[i]
         return a[n-1] + b[n-1];
     }
 
     public static void main(String[] args) {
+        //System.out.println("Number of String formed : " + countStrings(0));
+        System.out.println("Number of String formed : " + countStrings(1));
+        System.out.println("Number of String formed : " + countStrings(2));
         System.out.println("Number of String formed : " + countStrings(3));
+        System.out.println("Number of String formed : " + countStrings(4));
         System.out.println("Number of String formed : " + countStrings(5));
+        System.out.println("Number of String formed : " + countStrings(6));
+        System.out.println("Number of String formed : " + countStrings(7));
+        System.out.println("Number of String formed : " + countStrings(8));
+        System.out.println("Number of String formed : " + countStrings(9));
+        System.out.println("Number of String formed : " + countStrings(10));
 
     }
 
